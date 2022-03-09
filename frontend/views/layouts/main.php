9fe55a6bdde7105a2a98f6d959cf7feedc18e589
Alexandru Catalin Trandafir <alex@heavydots.com>
2022-03-09 22:27:38 +0100

    Analytics front

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -24,6 +24,17 @@ AppAsset::register($this);
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <?php $this->registerCsrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
+    
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-RJRS2D2E1E"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-RJRS2D2E1E');
+    </script>
+    
     <?php $this->head() ?>
 </head>
 <body class="d-flex flex-column h-100">
