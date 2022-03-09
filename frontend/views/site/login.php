a4b9323768b3057bfee5bb7b098f39ca4d623f65
Alexandru Catalin Trandafir <alex@heavydots.com>
2022-03-09 21:59:13 +0100

    Remove username from registration

--- frontend/views/site/login.php
+++ frontend/views/site/login.php
@@ -16,7 +16,10 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <div class="row">
         <div class="col-lg-5">
-            <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
+            <?php $form = ActiveForm::begin([
+                'id' => 'login-form',
+                'enableClientValidation' => false,
+                ]); ?>
 
                 <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
 
